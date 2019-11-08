b1b3579909506660fd7c6b3fdff87532f915dc03
Amin <aharna13@gmail.com>
2019-11-08 19:57:24 +0900

    FirstCommit

---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h3>Welcome!</h3>
+    <NLink to="/about">
+      About Page
+    </NLink>
+    <br>
+     <NLink to="/Amin">
+      Amins Page
+    </NLink>
+  </div>
+</template>
+
+<script>
+export default {
+  head: {
+    title: 'Home page'
+  }
+}
+</script>
